14016b800dc67655e7ff6b47010dc56f2c7c8a80
Jhonny Piñango <jhonnyleandro1@gmail.com>
2018-08-30 10:58:55 -0400

    asd

--- api/app/Models/Persons.php
+++ api/app/Models/Persons.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Users;
 
 class persons extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(Users::class,'person_id');
+    }
 }
